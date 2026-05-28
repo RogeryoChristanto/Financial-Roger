7eee08d6f5e258bfb465152111147be4863657fc
RogeryoChristanto <rogeryochristanto24@gmail.com>
2026-05-28 21:42:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1423,7 +1423,7 @@ def page_ai_advisor():
                 try:
                     # Inisialisasi Model Gemini Pro dengan System Instructions
                     model = genai.GenerativeModel(
-                        model_name='gemini-1.5-pro',
+                        model_name='gemini-1.5-flash',
                         system_instruction=build_ctx()
                     )
                     
